0ce2207b2419faf416d170b8aa954189bfd4138e
LiuHaolan <3140101098@zju.edu.cn>
2019-09-27 03:47:36 +0800

    2

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -36,7 +36,8 @@ static int add_monkey(unsigned long a)
 
 static int add_item(void){
 	printk(KERN_INFO "In thread\n");
-	return add_monkey(1);	
+//	return add_monkey(1);	
+	do_exit(0);
 //	return 0;
 }
 
@@ -45,30 +46,32 @@ static void destroy_list(void)
 	struct list_head *i, *n;
 	struct monkey_list *ple;
 
-	write_lock(&lock);
+//	write_lock(&lock);
 	list_for_each_safe(i, n, &my_list){
 		ple = list_entry(i, struct monkey, list);
 		list_del(i);
 		kfree(ple);
 	}
-	write_unlock(&lock);
+//	write_unlock(&lock);
 }
 
 static struct task_struct *thread_list[4];
 int thread_init(void){
 	printk(KERN_INFO "kthread init\n");
-	char* thread_name[4] = {"haolan1","haolan2","haolan3","haolan4"};
+//	char* thread_name[4] = {"haolan1","haolan2","haolan3","haolan4"};
 	int j=0;
 	int a = 0;	
 	for(j=0;j<4;j++){
 		
-		thread_list[j] = kthread_create(add_item, NULL, thread_name[j]);
+		thread_list[j] = kthread_run(add_item, NULL, "haolan%d",j);
 	}
+/*
 	for(a=0;a<4;j++){
 		if(thread_list[a]){
 			wake_up_process(thread_list[a]);
 		}
 	}
+*/
 	return 0;
 }
 
@@ -85,14 +88,14 @@ void thread_destroy(void){
 static int lab3_init(void){
 	printk(KERN_ALERT "Hello wuklab\n");
 	
-//	add_monkey(1);
-	thread_init();
+	add_monkey(1);
+//	thread_init();
 
 	return 0;
 }
 
 static void lab3_exit(void){
-	thread_destroy();
+//	thread_destroy();
 	destroy_list();
 	printk(KERN_ALERT "wuklab removed\n");
 }
